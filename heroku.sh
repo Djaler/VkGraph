e0171a2ddc69252090ac32886b4f3b7d79495a7c
Kirill Romanov <djaler1@gmail.com>
2017-05-17 18:58:05 +0300

    Возможность использования множества токенов

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-celery worker -A app.celery -c 1 &
+
+for TOKEN in $TOKENS
+do
+    export ACCESS_TOKEN=$TOKEN
+    celery worker -A app.celery -c 1 &
+done
+
 gunicorn -b "0.0.0.0:$PORT" app:app
