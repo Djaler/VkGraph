504212c918373e2c2356f7625a0513bf92cdf7cf
Kirill Romanov <djaler1@gmail.com>
2017-07-04 21:33:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ from uuid import uuid4
 TEMPLATES_AUTO_RELOAD = True
 CSRF_ENABLED = True
 SECRET_KEY = uuid4()
-CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.environ.get('REDISCLOUD_URL', 'redis://localhost:6379/0')
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+BROKER_TRANSPORT_OPTIONS = {
+    'max_connections': 20,
+}
